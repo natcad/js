4f5636b037e38ce878ad42a1b3d05a62fa4951c0
natcad <natashacadabon@gmail.com>
2024-03-24 16:59:59 -0300

    En este commit agregue arrays

--- proyecto1.js
+++ proyecto1.js
@@ -1,72 +1,107 @@
-let seguir =true;
-let frase;
-let ventana;
-let hornalla;
-let llave;
-let plantas;
-let billetera;
+//originalmente habia pensado en una estructura asi:
+let objetos = [
+  {
+    nombre: "llave",
+    pregunta: "agarrar la",
+    accion: "agarrado",
+    condicion: false,
+  },
+  {
+    nombre: "ventana",
+    pregunta: "cerrar la",
+    accion: "cerrado",
+    condicion: false,
+  },
+  {
+    nombre: "hornalla",
+    pregunta: "apagar la",
+    accion: "apagado",
+    condicion: false,
+  },
+  {
+    nombre: "plantas",
+    pregunta: "regar la",
+    accion: "regado",
+    condicion: false,
+  },
+  {
+    nombre: "billetera",
+    pregunta: "agarrar la",
+    accion: "agarrado",
+    condicion: false,
+  },
+  { 
+    nombre: "luz", 
+    pregunta: "apagar la", 
+    accion: "apagado", 
+    condicion: false }
+];
+//pero me genero conflictos en el momento de modificar cada objeto, por eso descartaria el array
+//Me parece mas optimo crear la clase objeto.
 
+function jugar() {
+  //Funcion principal
 
-function jugar(){
-    alert("Estas a punto de empezar a jugar! El objetivo es salir de casa");
-    alert("Abrí la consola");
-    while (seguir){
-        console.log('Es hora de irte!');
+  let seguir = true;
 
-        llave =accion('llave', 'agarrado', 'Agarraste la');
-        if (!seguir) break; 
-        
-        ventana =accion('ventana', 'cerrado', 'Cerraste la');
-        if (!seguir) break; 
-        
-        billetera =accion('billetera', 'agarrado', 'Agarraste la');
-        if (!seguir) break; 
-        
-        hornalla =accion('hornalla', 'apagado', 'Apagaste la');
-        if (!seguir) break; 
-        
-        plantas =accion('planta', 'regado', 'Regaste la');
-        if (!seguir) break; 
-        
-        
+  alert("Estas a punto de empezar a jugar! El objetivo es salir de casa");
+  alert("Abrí la consola");
+
+  //esta parte de codigo con objeto es mas simple y no necesito la funcion extra reemplazar()
+  for (const objeto of objetos) {
+    if (!seguir) break;
+    let nuevoObj = accion(objeto);
+    if (nuevoObj !== NULL) {
+      reemplazar(nuevoObj);
     }
+    seguir = irse();
+  }
 
-    console.log('Has salido de tu casa!');
-    
-    if(ventana && llave && plantas && billetera && hornalla){
-        alert("tu casa está segura y tienes todo que tengas un lindo día! :)");
-    }else{
-        alert("Cuidado!! Tu casa no está segura y/o te faltan cosas");
-    }
-    seguir=true;
-    }
+  console.log("Has salido de tu casa!");
 
-function chequear(frase, condicional){
-    if (condicional.toLowerCase() == "si"){
-        console.log(`Has ${frase}}!`);
-       irse();
-       return true;
-    }else{
-        console.log()
-        return false;
+  let todasVerdaderas = objetos.every((objeto) => objeto.condicion);
+  if (todasVerdaderas) {
+    alert("No has olvidado nada, tu casa está segura! Buena suerte en tu día.");
+  } else {
+    for (const objeto of objetos) {
+      alert("Tu casa no está segura");
+      if (!objeto.condicion) {
+        alert(`Te has olvidado de ${objeto.accion} ${objeto.nombre}`);
+      }
     }
+  }
 }
 
-function irse(){
-    console.log("Quieres irte?");
-    let ir = prompt("ingrese si o no");
-    if (ir.toLowerCase() == "si"){
-        seguir = false;
-    }
-
+function reemplazar(obj) {
+  //funcion que reemplaza del array objeto inicial con el objeto modificado
+  const i = objetos.findIndex((o) => o.nombre === obj.nombre);
+  if (i !== -1) {
+    objetos[i] = obj;
+  }
 }
 
-function accion(nombre, accion, pregunta){
-    console.log( `${pregunta} ${nombre}?`);
-    let respuesta = prompt("Ingrese si o no");
-    let frase = `${accion} la ${nombre}`;
-    return chequear(frase, respuesta);
+function irse() {
+  //funcion para salir del juego esto no va a estar cuando pueda utilizar eventos.
+  console.log("Quieres irte?");
+  let ir = prompt("ingrese si o no");
+  if (ir.toLowerCase() == "si") {
+    return false;
+  }
 }
 
+function accion(objeto) {
+  //consulta para realizar o no la accion.
+  console.log(`Quieres ${objeto.pregunta} ${objeto.nombre}?`);
+  let respuesta = prompt("Ingrese si o no");
+  if (respuesta.toLowerCase() == "si") {
+    console.log(`Has ${objeto.accion} la ${objeto.nombre}`);
+    objeto.condicion = true;
+    return objeto;
+  } else {
+    console.log(`No has ${objeto.accion} la ${objeto.nombre}`);
+    objeto.condicion = false;
+    return null;
+  }
+}
 
 jugar();
